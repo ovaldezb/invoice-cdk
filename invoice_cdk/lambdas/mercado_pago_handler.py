808e907054e4bb542ba77c4aec21e6953b4e247a
omar valdez <omar.valdez.becerril@gmail.com>
2026-01-24 19:51:29 -0600

    binary mode

--- invoice_cdk/lambdas/mercado_pago_handler.py
+++ invoice_cdk/lambdas/mercado_pago_handler.py
@@ -63,7 +63,8 @@ def handler(event, context):
                 "failure": f"{origin}/dashboard" if origin else "http://localhost:4200/dashboard",
                 "pending": f"{origin}/dashboard" if origin else "http://localhost:4200/dashboard"
             },
-            # "auto_return": "approved"
+            # "auto_return": "approved",
+            "binary_mode": True
         }
 
         logger.info("Sending preference data: %s", json.dumps(preference_data))
